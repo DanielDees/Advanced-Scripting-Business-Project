d31c06f914a580328dd8d1b75fc642f4a3a0edd4
Dylan Porter <33324680+LANPort@users.noreply.github.com>
2017-11-16 18:02:30 -0500

    updated admin functions
    - adduser function newly added
    - updated verify_user function
    - updated login page

---
+++ fhp_adduser.php
@@ -0,0 +1,56 @@
+<?php
+    // Author:      Dylan Porter
+    // Date:        11/16/2017
+    // Purpose:     Add a user to the database
+
+
+    // TODO: change POST variables to match the form
+    // TODO: redirect page to the correct place
+    // TODO: add error catching for matching passwords and valid inputs
+
+
+    // open connection to database
+    require_once('connect.php');
+    
+    // grab all posted variables
+    $firstname = $_POST['first'];
+    $lastname = $_POST['last'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $repassword = $_POST['repassword'];
+    $account = $_POST['account'];
+    
+    // sanitize user input
+    $firstname = trim($firstname);
+    $lastname = trim($lastname);
+    $email = trim($email);
+    $username = trim($username);
+    $password = trim($password);
+    $repassword = trim($repassword);
+    $account = trim($account);
+    
+    $firstname = mysqli_real_escape_string($conn, $firstname);
+    $lastname = mysqli_real_escape_string($conn, $lastname);
+    $email = mysqli_real_escape_string($conn, $email);
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
+    $repassword = mysqli_real_escape_string($conn, $repassword);
+    $account = mysqli_real_escape_string($conn, $account);
+    $salt = strtoupper(bin2hex(random_bytes(20)));
+    
+    // update database with the new user information
+    $sql = 'INSERT INTO users (Id, first_name, last_name, email, username, account_type)
+        VALUES (NULL, "' . $firstname . '", "' . $lastname . '", "' . $email . '", "' . $username . '", "' . strtoupper($account) . '")';
+    
+    $result = $conn->query($sql);
+    
+    $sql = 'INSERT INTO logins (id, username, password_hash, password_salt)
+        VALUES (NULL, "' . $username . '", PASSWORD("' . $password . '"), "' . $salt . '" )';
+    
+    $result = $conn->query($sql);
+    
+    mysqli_close($conn);
+    
+    //header("Location: REDIRECT");
+?>
