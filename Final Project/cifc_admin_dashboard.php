d92926115d41a988ea498f06d7738c9c09429c30
AR97 <33634717+AR97@users.noreply.github.com>
2017-11-17 19:50:19 -0500

    Add files via upload
    For admin Dashboard page

---
+++ Final Project/cifc_admin_dashboard.php
@@ -0,0 +1,45 @@
+<html>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+<link rel="stylesheet" href="fhp_test.css">
+<title>CIFC Admin</title>
+</head>
+
+<body id="home">	
+
+	<div id="banner-top">
+		<img id="title-img" src="fhp_logo.png"></img>
+		
+		<h2 id="admin-subtitle">Administration: User Type</h2>
+
+		<ul>
+			<!--Dashboard Tab-->
+			<a href="cifc_admin_dashboard.php"><li class="dashboard">Dashboard</li></a>	
+			<!--Articles Tab-->
+			<a href="cifc_admin_article_view.php"</a><li>Articles</li>		
+			
+		</ul>
+	</div>
+	
+	<div id="block-holder">
+		<div id="action-block">
+			<h3 id="block-text">Add<br>User</h3>
+			<img id="block-icon" src="user_add_icon.png"></img>
+		</div>
+	
+	
+		<div id="action-block">
+			<h3 id="block-text">Publish<br>Article</h3>
+			<img id="block-icon" src="article_publish_icon.png"></img>
+		</div>
+		
+		<div id="action-block">
+			<h3 id="block-text">Create<br>Article</h3>
+			<img id="block-icon" src="article_add.png"></img>
+		</div>
+	</div>
+	
+</html>
+</body>
