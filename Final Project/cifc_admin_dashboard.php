2ea1cd403b6a659460f5259867ef447e324ac319
AR97 <33634717+AR97@users.noreply.github.com>
2017-11-17 21:05:04 -0500

    Most Recent Files 11/17/17 9:00pm

--- Final Project/cifc_admin_dashboard.php
+++ Final Project/cifc_admin_dashboard.php
@@ -18,27 +18,32 @@
 			<!--Dashboard Tab-->
 			<a href="cifc_admin_dashboard.php"><li class="dashboard">Dashboard</li></a>	
 			<!--Articles Tab-->
-			<a href="cifc_admin_article_view.php"</a><li>Articles</li>		
+			<a href="cifc_admin_article_view.php"><li>Articles</li></a>
 			
 		</ul>
 	</div>
 	
 	<div id="block-holder">
+		<a href="#">
 		<div id="action-block">
 			<h3 id="block-text">Add<br>User</h3>
 			<img id="block-icon" src="user_add_icon.png"></img>
 		</div>
+		</a>
 	
-	
-		<div id="action-block">
+		<a href="#">
+		<div id="action-block-center">
 			<h3 id="block-text">Publish<br>Article</h3>
 			<img id="block-icon" src="article_publish_icon.png"></img>
 		</div>
+		</a>
 		
+		<a href="cifc_admin_add_article_form.html">
 		<div id="action-block">
 			<h3 id="block-text">Create<br>Article</h3>
 			<img id="block-icon" src="article_add.png"></img>
 		</div>
+		</a>
 	</div>
 	
 </html>
